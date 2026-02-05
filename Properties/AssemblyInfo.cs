95d449a69bc7ceb3e029e533c4fa670087448857
Aignistar <github@aigner-rlp.de>
2026-02-05 11:01:09 +0100

    Reapply "Initial release."
    This reverts commit 7b201eb6d10af142662c9061d04e60d1e9eefd5e.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using Frosty.Core.Attributes;
+using RevertFolderPlugin;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
+
+[assembly: PluginDisplayName("Revert Folder")]
+[assembly: PluginAuthor("Aigni")]
+[assembly: PluginVersion("1.0.0.0")]
+
+[assembly: RegisterDataExplorerContextMenu(typeof(RevertFolderContextMenuItem))]
+
+[assembly: AssemblyTitle("Revert Folder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aigni")]
+[assembly: AssemblyProduct("Revert Folder")]
+[assembly: AssemblyCopyright("Copyright © Aigni 2026")]
+
+[assembly: AssemblyVersion("1.0.0")]
